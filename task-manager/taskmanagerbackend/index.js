3502c8a75d8cbd4619abbfadf7e47b6083a701cb
Rimjhim Khanna <rimjhimkhanna@Rimjhims-MacBook-Air.local>
2025-01-23 21:30:27 +0530

    23jan25

---
+++ task-manager/taskmanagerbackend/index.js
@@ -0,0 +1,29 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const userRoutes = require('./routes/userRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+
+require('dotenv').config();
+require('./db');
+const PORT = 8000;
+
+app.use(bodyParser.json());
+app.use('/users', userRoutes);
+app.use('/tasks', taskRoutes);
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Task Manager API is working!'
+    })
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
+
+
+//npm install --save-dev nodemon
+//npx nodemon index.js
+//for installing nodemon locally
